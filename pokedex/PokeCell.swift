d9a994b806744a4a2348495f216cd8787b1974fc
Malkiel Shaul <malkiel@h-MacBook-Pro-sl-Malkiel.local>
2017-04-27 13:16:51 +0300

    search was installed

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,37 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Malkiel Shaul on 27.4.2017.
+//  Copyright © 2017 Malkiel Shaul. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+    
+}
---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Malkiel Shaul on 27.4.2017.
+//  Copyright © 2017 Malkiel Shaul. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
